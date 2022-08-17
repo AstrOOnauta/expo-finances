c9f5d828bee60cd83e02bdafc86d5db12fb303ba
Will <willian.ralf.rn@gmail.com>
2022-08-17 12:08:05 -0300

    feat: expo-fonts + global styles config - fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+
 import Dashboard from './src/screens/Dashboard';
 import theme from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,5 +6,10 @@ export default {
     yellow:"#FBB034",
     green:"#33CC95",
     red:"#E52E4D"
+  },
+  fonts:{
+    normal:"Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    bold: "Poppins_700Bold"
   }
 }
