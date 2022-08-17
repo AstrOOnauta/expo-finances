4d41d74aa9ac9e4628ad73f7739b4f584bea3c9e
Will <willian.ralf.rn@gmail.com>
2022-08-17 10:45:14 -0300

    chore: dashboard component with styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { DashboardContainer } from './styles';
+
+export default function Dashboard() {
+  return (
+    <DashboardContainer>
+      <Text>Dashboard</Text>
+    </DashboardContainer>
+  );
+}
